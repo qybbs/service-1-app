e766376aa6d2f87eba7a87bf2d0c1a21db235a9f
Muhammad Iqbal Ghozy <iqbal.gozy@gmail.com>
2024-05-18 17:19:04 +0700

    update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Ini service 1');
+  res.send('Ini service 1 yaa');
 });
 
 const PORT = process.env.PORT || 8080;
